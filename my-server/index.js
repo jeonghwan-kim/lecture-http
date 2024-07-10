3e4cff6041b62c1e8382e1fe11432a0bdaff2546
김정환 <ej88ej@gmail.com>
2024-07-10 22:57:38 +0900

    mokpo

--- my-server/index.js
+++ my-server/index.js
@@ -1,23 +1,24 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 // 요청한 파일을 응답한다.
-
 function handler(req, res) {
-  const filename = req.url.replace(/^\//, '')
-  console.log(filename)
-  const filepath = path.resolve(__dirname, 'public', filename)
+  const filename = req.url.replace(/^\//, "");
+  console.log(filename);
+  const filepath = path.resolve(__dirname, "public", filename);
   fs.readFile(filepath, (err, data) => {
     if (err) {
-      console.error(err)
-      res.end('Error')
+      console.error(err);
+      res.end("Error");
       return;
     }
 
-    res.end(data)
-  })
+    res.end(data);
+  });
 }
 
-const server = http.createServer(handler)
-server.listen(3000, ()=> console.log('server is running ::3000'))
+const server = http.createServer(handler);
+// server.keepAliveTimeout = 1000; // Keep-Alive 타임아웃 설정 (기본: 5000 밀리초)
+
+server.listen(3000, () => console.log("server is running ::3000"));
