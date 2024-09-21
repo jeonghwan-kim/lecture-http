30063f4feb199e140a4835291ced1b8d1a84242c
김정환 <ej88ej@gmail.com>
2024-09-21 10:14:40 +0900

    1.2

--- 01/my-client/index.js
+++ 01/my-client/index.js
@@ -1,26 +1,30 @@
-const http = require('http')
+const http = require("http");
 
-// URL을 받아서 요청, 응답한 결과를 출력한다.
-const url = process.argv[2]
+// 명령어 인자로 받은 url
+const url = process.argv[2];
 
+// url 인자가 없으면 오류를 출력하고 종료합니다.
 if (!url) {
-  console.error('Usage: node my-client/index.js <url>')
+  console.error("Usage: node my-client/index.js <url>");
+  process.exit();
 }
 
-const options = new URL(url)
+// 인자로 접속하는 url 객체를 준비합니다.
+const options = new URL(url);
 
-const req = http.request(options, res => {
-  const data = []
-  res.on('data', chunk => {
-    data.push(chunk.toString())
-  })
-  res.on('end', ()=> {
-    console.log(data.join(''))
-  })
-})
+// http 모듈의 request 함수로 서버에 요청을 보내는 reqeust 객체를 구성합니다.
+const req = http.request(options, (res) => {
+  const data = [];
+  res.on("data", (chunk) => {
+    data.push(chunk.toString());
+  });
+  res.on("end", () => {
+    console.log(data.join(""));
+  });
+});
 
-req.on('error', err => {
-  console.error(err)
-})
+req.on("error", (err) => {
+  console.error(err);
+});
 
-req.end()
+req.end();
--- 01/my-server/index.js
+++ 01/my-server/index.js
@@ -2,23 +2,31 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// 요청한 파일을 응답한다.
-function handler(req, res) {
-  const filename = req.url.replace(/^\//, "");
-  console.log(filename);
-  const filepath = path.resolve(__dirname, "public", filename);
+// 요청한 파일을 응답하는 함수
+function static(req, res) {
+  // 요청한 파일의 경로를 계산합니다.
+  const filepath = path.join(__dirname, "public", req.url);
+
+  // 요청한 파일을 읽습니다.
   fs.readFile(filepath, (err, data) => {
+    // 파일을 읽지 못한 경우
     if (err) {
-      console.error(err);
-      res.end("Error");
+      res.end("Not Found");
       return;
     }
 
+    // 파일을 읽고 내용을 응답합니다.
     res.end(data);
   });
 }
 
-const server = http.createServer(handler);
-// server.keepAliveTimeout = 1000; // Keep-Alive 타임아웃 설정 (기본: 5000 밀리초)
+// 이 컨텐츠로 요청을 처리할 핸들러 함수를 만들었습니다.
+function handler(req, res) {
+  static(req, res);
+}
 
+// 위 핸들러로 http 서버 객체를 하나 만들었다.
+const server = http.createServer(handler);
+
+// 서버는 3000번 포트에서 요청을 기다리게 됩니다.
 server.listen(3000, () => console.log("server is running ::3000"));
