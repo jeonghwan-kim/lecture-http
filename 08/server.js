c39d345fcc4e6c23cfb32a4973e2fcbd631b1f06
김정환 <ej88ej@gmail.com>
2024-10-01 08:37:01 +0900

    8.1

--- 08/server.js
+++ 08/server.js
@@ -1,66 +1,31 @@
-const http = require("http")
-const fs = require("fs")
+const http = require("http");
+const path = require("path");
+const static = require("../shared/serve-static");
 
-function hello(req, res) {
+function success(req, res) {
   res.writeHead(200, {
-    "content-type": "text/plain",
-  })
-  res.end("Hello world")
+    "content-type": "application/json",
+  });
+  res.end(JSON.stringify({ result: "success" }));
 }
 
 function fail(req, res) {
   res.writeHead(400, {
-    "content-type": "text/plain",
-  })
-  res.end("Fail")
-}
-
-function index(req, res) {
-  fs.readFile("./index.html", (err, content) => {
-    if (err) {
-      res.writeHead(500)
-      res.end("Error")
-      return
-    }
-
-    res.writeHead(200, {
-      "content-type": "text/html",
-    })
-    res.end(content)
-  })
-}
-function js(req, res) {
-  fs.readFile("./main.js", (err, content) => {
-    if (err) {
-      res.writeHead(500)
-      res.end("Error")
-      return
-    }
-
-    res.writeHead(200, {
-      "content-type": "application/javascript",
-    })
-    res.end(content)
-  })
-}
-
-function notFound(req, res) {
-  res.writeHead(404)
-  res.end("페이지를 찾을 수 없습니다.")
+    "content-type": "application/json",
+  });
+  res.end(JSON.stringify({ result: "fail" }));
 }
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
 
-  const { pathname } = new URL(req.url, `http://${req.headers.host}`)
+  if (pathname === "/api/success") return success(req, res);
+  if (pathname === "/api/fail") return fail(req, res);
 
-  if (pathname === "/hello") return hello(req, res)
-  if (pathname === "/fail") return fail(req, res)
-  if (pathname === "/main.js") return js(req, res)
-  if (pathname === "/") return index(req, res)
-  return notFound(req, res)
-})
+  // 정적 파일 요청을 처리한다.
+  static(path.join(__dirname, "public"))(req, res);
+});
 
 server.listen(3000, () => {
-  console.log("server is running ::3000")
-})
+  console.log("server is running ::3000");
+});
