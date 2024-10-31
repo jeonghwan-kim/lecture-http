2c3122e197190897fd1f44e2f48c4a40019274b4
김정환 <ej88ej@gmail.com>
2024-11-01 06:50:25 +0900

    ch01

--- ch01/my-client/index.js
+++ ch01/my-client/index.js
@@ -1,1 +1,23 @@
-// todo 
+const http = require("http");
+
+const url = process.argv[2];
+if (!url) {
+  console.error("Usage: node my-client/index.js <url>");
+  process.exit();
+}
+const options = new URL(url);
+
+const handler = (res) => {
+  const data = [];
+
+  res.on("data", (chunk) => {
+    data.push(chunk.toString());
+  });
+
+  res.on("end", () => {
+    console.log(data.join(""));
+  });
+};
+
+const req = http.request(options, handler);
+req.end();
--- ch01/my-server/index.js
+++ ch01/my-server/index.js
@@ -1,1 +1,22 @@
-// todo 
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const static = (req, res) => {
+  const filepath = path.join(__dirname, "public", req.url);
+
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      res.write("Not Found\n");
+      res.end();
+      return;
+    }
+
+    res.write(data);
+    res.end();
+  });
+};
+const handler = (req, res) => static(req, res);
+
+const server = http.createServer(handler);
+server.listen(3000, () => console.log("server is running ::3000"));
