ec111694eb0d73a7c273ac222ee4fad8258c80c4
김정환 <ej88ej@gmail.com>
2024-11-02 13:19:47 +0900

    init ch05

---
+++ shared/serve-static.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const path = require("path");
+
+const serveStatic = (root) => {
+  return (req, res) => {
+    const filepath = path.join(root, req.url === "/" ? "/index.html" : req.url);
+
+    fs.readFile(filepath, (err, data) => {
+      if (err) {
+        if (err.code === "ENOENT") {
+          res.statusCode = 404;
+          res.write("Not Found\n");
+          res.end();
+          return;
+        }
+
+        res.statusCode = 500;
+        res.write("Internal Server Error\n");
+        res.end();
+        return;
+      }
+
+      const ext = path.extname(filepath).toLowerCase();
+      let contentType = "text/html";
+      switch (ext) {
+        case ".html":
+          contentType = "text/html";
+          break;
+        case ".js":
+          contentType = "text/javascript";
+          break;
+        case ".css":
+          contentType = "text/css";
+          break;
+        case ".png":
+          contentType = "image/png";
+          break;
+        case ".json":
+          contentType = "application/json";
+          break;
+        case ".otf":
+          contentType = "font/otf";
+          break;
+        default:
+          contentType = "application/octet-stream";
+      }
+      res.setHeader("Content-Type", contentType);
+
+      res.write(data);
+      res.end();
+    });
+  };
+}
+
+module.exports = serveStatic;
