8c292328950762feff2a14442ef1c09978b9824a
김정환 <ej88ej@gmail.com>
2024-11-03 11:20:03 +0900

    ch06

--- ch06/public/script.js
+++ ch06/public/script.js
@@ -5,8 +5,35 @@ const init = () => {
   loginForm.addEventListener("submit", handleSubmit);
 };
 
-const handleSubmit = (event) => {
-  // todo
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const jsonData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData),
+    });
+
+    if (!response.ok) {
+      alert("API Error");
+      return;
+    }
+
+    const body = await response.json();
+    alert("Success: " + body.authenticated);
+  } catch (error) {
+    alert("Network Error");
+  }
 };
 
 document.addEventListener("DOMContentLoaded", init);
--- ch06/server.js
+++ ch06/server.js
@@ -2,7 +2,29 @@ const http = require("http");
 const path = require("path");
 const static = require("../shared/serve-static");
 
+const handleLogin = (req, res) => {
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body = body + chunk.toString();
+  });
+
+  req.on("end", () => {
+    const { email, password } = JSON.parse(body);
+    const authenticated = email === "myemail" && password === "mypassword";
+
+    res.statusCode = authenticated ? 200 : 401;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify({ authenticated }));
+    res.end();
+  });
+};
+
 const handler = (req, res) => {
+  let { pathname } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (pathname === "/login") return handleLogin(req, res);
+
   static(path.join(__dirname, "public"))(req, res);
 };
 
