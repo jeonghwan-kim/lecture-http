dbe8a5e8f71c872296d83c4f5468a8e01295638e
김정환 <ej88ej@gmail.com>
2024-11-04 10:41:47 +0900

    ch11

--- ch11/public/script.js
+++ ch11/public/script.js
@@ -1,3 +1,10 @@
+const subscribe = () => {
+  eventSource = new EventSource("/subscribe");
+  eventSource.addEventListener("message", function (e) {
+    render(JSON.parse(e.data));
+  });
+};
+
 const render = (message) => {
   const messageElement = document.createElement("div");
   const { text } = message;
@@ -7,6 +14,6 @@ const render = (message) => {
 };
 
 const init = () => {
-  // todo
+  subscribe();
 };
 document.addEventListener("DOMContentLoaded", init);
--- ch11/server.js
+++ ch11/server.js
@@ -4,11 +4,63 @@ const static = require("../shared/serve-static");
 const Message = require("../shared/message");
 
 let message = null;
+let waitingClients = [];
+
+const subscribe = (req, res) => {
+  res.setHeader("content-type", "text/event-stream");
+  res.write("\n");
+
+  waitingClients.push(res);
+
+  req.on("close", () => {
+    waitingClients = waitingClients.filter((client) => client !== res);
+  });
+};
+
+const update = (req, res) => {
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body = body + chunk.toString();
+  });
+
+  req.on("end", () => {
+    const { text } = JSON.parse(body);
+
+    if (!text) {
+      res.statusCode = 400;
+      res.setHeader("content-type", "application/json");
+      res.write(
+        JSON.stringify({
+          error: "text 필드를 채워주세요",
+        })
+      );
+      res.end();
+      return;
+    }
+
+    const message = new Message(text);
+
+    for (const waitingClient of waitingClients) {
+      waitingClient.write(
+        [
+          `retry: 10000\n`,
+          `id: ${message.timestamp}\n`,
+          `data: ${message.toString()}\n\n`,
+        ].join("")
+      );
+    }
+
+    res.write(`${message}`);
+    res.end();
+  });
+};
 
 const handler = (req, res) => {
   const { pathname } = new URL(req.url, `http://${req.headers.host}`);
 
-  // todo
+  if (pathname === "/subscribe") return subscribe(req, res);
+  if (pathname === "/update") return update(req, res);
 
   static(path.join(__dirname, "public"))(req, res);
 };
