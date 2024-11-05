f0597d7d0822dba43179a099f17a9447c1a1b239
김정환 <ej88ej@gmail.com>
2024-11-05 19:47:46 +0900

    ch14

--- ch14/public/script.js
+++ ch14/public/script.js
@@ -1,4 +1,15 @@
 const init = async () => {
-  console.log("init");
+  const res = await fetch("http://localhost:3001/resource.json", {
+    headers: {
+      // 안전하지 않은 헤더를 사용한다.
+      "X-Foo": "foo",
+    },
+    method: "PUT",
+  });
+
+  const data = await res.text();
+  const jsonEl = document.createElement("pre");
+  jsonEl.textContent = data;
+  document.body.appendChild(jsonEl);
 };
 document.addEventListener("DOMContentLoaded", init);
--- ch14/server.js
+++ ch14/server.js
@@ -3,8 +3,12 @@ const path = require("path");
 const static = require("../shared/serve-static");
 
 const handler = (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Headers", "X-Foo");
+  res.setHeader("Access-Control-Allow-Methods", "PUT");
   static(path.join(__dirname, "public"))(req, res);
 };
 
 const server = http.createServer(handler);
-server.listen(3000, () => console.log("server is running ::3000"));
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`server is running ::${port}`));
